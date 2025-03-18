f28d2a1f36ee8f94e9321d0084fb1578565ec270
Valecaca2020 <dianaspfc20@gmail.com>
2025-03-18 08:26:53 -0300

    Update app.js
    Meu projeto amigo secreto.

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-let = ('digite o nome dos seus amigos');
+let listaDeAmigos=[]
+
+function adicionarNomeDeAmigos(){
+        amigosnome = inputAmigo.value.trim(); 
+
+        if (nome === '') {
+            alert('Por favor, digite um nome de um amigo.'); 
+            return; 
+    }
+
+amigos.push(amigos)
+
+    const itemLista = document.createElement('li'); 
+    itemLista.textContent = nome; 
+    listaAmigos.appendChild(itemLista); 
+
+    inputAmigo.value = '';
+    inputAmigo.focus(); 
+
+    function sortearNomeDeAmigo (){
+        if (nomes.length === 0) {
+            alert('A lista está vazia! Adicione nomes antes de sortear.');
+            return;
+    }
+
+         indiceAleatorio = Math.floor(Math.random() * nomes.length);{
+    
+         const nomeDeAmigoSorteado = amigos[indiceAleatorio];
+
+         resultado.textContent = `O amigo secreto sorteado é: ${nomeSorteado}`;
+
+        
